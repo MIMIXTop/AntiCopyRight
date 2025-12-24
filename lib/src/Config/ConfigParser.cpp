7870f57d90338788676f6e4c1f92340515e3216a
MIMIXTop <maksvech812@gmail.com>
2025-12-24 21:19:13 +0300

    feat(config): add ConfigParser class implementation
    - Introduces new ConfigParser class with header and implementation files
    - Empty class structure ready for configuration parsing functionality
    - Files created in lib/src/Config/ directory

---
+++ lib/src/Config/ConfigParser.cpp
@@ -0,0 +1,8 @@
+//
+// Created by mimixtop on 24.12.2025.
+//
+
+#include "ConfigParser.hpp"
+
+namespace Util {
+} // Util
---
+++ lib/src/Config/ConfigParser.hpp
@@ -0,0 +1,16 @@
+//
+// Created by mimixtop on 24.12.2025.
+//
+
+#ifndef ANTICOPYRIGHT_CONFIGPARSER_HPP
+#define ANTICOPYRIGHT_CONFIGPARSER_HPP
+
+namespace Util {
+
+class ConfigParser {
+
+};
+
+} // Util
+
+#endif //ANTICOPYRIGHT_CONFIGPARSER_HPP
